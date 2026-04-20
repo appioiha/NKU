4632d492da1d12489f568ebf32a8d7495fc12d7c
appioiha <zyj653111025@qq.com>
2026-04-20 16:13:03 +0800

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,49 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 AccountBook.rc 使用
+//
+#define IDD_DLG_ADD                     9
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_ACCOUNTBOOK_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     134
+#define IDD_DLG_STATISTICS              134
+#define IDC_BTN_ADD                     1000
+#define IDC_BTN_STAT                    1001
+#define IDC_FRAME                       1002
+#define IDC_RADIO_IN                    1003
+#define IDC_RADIO_OUT                   1004
+#define IDC_DATETIME                    1005
+#define IDC_EDIT_MONEY                  1006
+#define IDC_RADIO_CLOTH                 1007
+#define IDC_RADIO_FOOD                  1008
+#define IDC_RADIO_HOME                  1009
+#define IDC_RADIO_TRAVEL                1010
+#define IDC_RADIO_EDU                   1012
+#define IDC_RADIO_PLAY                  1013
+#define IDC_BTN_SAVE                    1014
+#define IDC_BTN_DELETE                  1015
+#define IDC_LIST_RECORD                 1016
+#define IDCANCEL                        1017
+#define IDC_DATETIME_START              1018
+#define IDC_DATETIME_END                1020
+#define IDC_BTN_QUERY                   1021
+#define IDC_LIST1                       1022
+#define IDC_LIST_STAT                   1023
+#define IDC_STATIC_INCOME               1024
+#define IDC_STATIC_OUTCOME              1025
+#define IDC_STATIC_BALANCE              1026
+#define IDC_STATIC_PIE                  1027
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1028
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
